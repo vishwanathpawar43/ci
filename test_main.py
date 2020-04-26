9fa24399c92c1e6b349c543476d7a510ea26cbbb
Vishwanath Pawar <64090069+vishwanathpawar43@users.noreply.github.com>
2020-04-26 19:35:28 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 import main
 def test_multipy():
-  result = main.multipy(3 , 4)
-  print("running")
-  print("hi")
-  assert result == 12
+  result = main.multipy(3, 4)
+  print("running tests...")
+ 
+  assert result == 12 # throws error if result != 12
